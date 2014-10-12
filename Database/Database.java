47656df0e0f2cf158252af6188caae9b0eed085f
michel94 <miguel.duarte20@hotmail.com>
2014-10-12 02:01:29 +0100

    Added and integrated mysql connector.

--- Database/Database.java
+++ Database/Database.java
@@ -2,17 +2,30 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class Database extends UnicastRemoteObject implements DatabaseInterface{
-	protected Database() throws RemoteException {
+	public Connection connection;
+
+	protected Database() throws RemoteException, SQLException {
 		super();
 		//initialize JDBC
+		try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Database started");
+        String url = "jdbc:mysql://localhost:3306/test";
+        connection = DriverManager.getConnection(url,"root","");
+        System.out.println("Connected");
 	}
+
 	public ArrayList<String> getMeetings(){
 
 		return new ArrayList<String>();
 	}
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws RemoteException, SQLException {
 		DatabaseInterface di = new Database();
 		LocateRegistry.createRegistry(1099).rebind("database", di);
 		System.out.println("Database ready...");
