4cef32fef26b6d3dfda83dcde97ca6754caf3c3b
michel94 <miguel.duarte20@hotmail.com>
2014-10-18 23:15:51 +0100

    Added Udp Fail Over. Still needs stonith.

--- Server/Server.java
+++ Server/Server.java
@@ -13,18 +13,37 @@ import java.util.*;
 public class Server{
 	private int clientNumber = 0;
 	private int serverPort = 6000;
+	private Boolean second;
 
-	protected Server() throws RemoteException, MalformedURLException, NotBoundException {
+	protected Server(boolean s, int port) throws RemoteException, MalformedURLException, NotBoundException {
 		super();
+
+		serverPort = port;
+		second = s;
+
+
+		UdpConnection u = new UdpConnection(second);
+
+
+		if(second)
+			try{
+				System.out.println("Waiting");
+				synchronized(second){
+					second.wait();	
+				}
+				System.out.println("Ended Waiting");
+			}catch(InterruptedException e){e.printStackTrace();}
+
 
 		ObjectOutputStream opipeout;
 		ObjectInputStream opipein;
 		DatabaseInterface database = (DatabaseInterface) Naming.lookup("database");
-		
+
+
 		Socket clientSocket;
 		try{
 
-			System.out.println("Listening to port 6000");
+			System.out.println("Listening to port " + serverPort);
 			ServerSocket listenSocket = new ServerSocket(serverPort);
 			System.out.println("Server ready");
 			
@@ -34,7 +53,6 @@ public class Server{
 				// create communication between client thread and server thread
 				PipedOutputStream pipeout = new PipedOutputStream();
 				PipedInputStream pipein = new PipedInputStream(pipeout);
-				
 				
 				opipeout = new ObjectOutputStream(new DataOutputStream(pipeout));
 				opipein = new ObjectInputStream(new DataInputStream(pipein));
@@ -52,8 +70,32 @@ public class Server{
 	}
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
-		Server server = new Server();
+		boolean primary=false, second=false;
+		int port=6000;
+
+		for(int i=0; i<args.length; i++){
+			if(args[i].equals("-p"))
+				primary = true;
+			else if(args[i].equals("-s"))
+				second = true;
+			else{
+				try{
+					port = Integer.parseInt(args[i]);
+				}catch(NumberFormatException e){
+					;
+				}
+			}
+		}
+		if(primary && second){
+			System.out.println("The server cannot be primary and secondary at the same time");
+			return;
+		}
+
+
+		Server server = new Server(second, port);
 		
+		
+
 	}
 
 }
---
+++ Server/UdpConnection.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class UdpConnection implements Runnable {
+	private int udpPort = 5000;
+	private Boolean secondary;
+	private boolean sec;
+	private int losses=0;
+
+	public UdpConnection(Boolean s){
+		secondary = s;
+		sec = s;
+
+		Thread t = new Thread(this, "pong");
+		
+		t.start();
+
+	}
+
+	public void run() {
+		DatagramSocket socket = null;
+		String s;
+		while(true)
+		if(!sec){ //primary
+			
+			try{
+				socket = new DatagramSocket(udpPort);
+
+				while(true){
+					byte[] buffer = new byte[1000]; 			
+					DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+					socket.receive(request);
+					s = new String(request.getData(), 0, request.getLength());	
+					System.out.println("Server Received: " + s);
+
+					DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+					socket.send(reply);
+				}
+			}catch (SocketException e){
+				;
+			}catch (IOException e) {
+				;
+			}finally {
+				;
+			}
+		}else{ // secondary
+			try {
+				socket = new DatagramSocket();
+				socket.setSoTimeout(5000);
+
+				while(true){
+					
+					if(losses > 3){
+						
+						synchronized(secondary){
+							System.out.println("synchronized");
+							sec = false;
+							secondary.notify();
+							break;
+						}
+					}
+
+					Thread.sleep(1000);
+					byte [] m = (new String("ping")).getBytes();
+					InetAddress aHost = InetAddress.getByName("localhost");
+					DatagramPacket request = new DatagramPacket(m, m.length, aHost, udpPort);
+
+					System.out.println("Sending ping");
+					socket.send(request);
+
+					System.out.println("Waiting pong");
+					byte[] buffer = new byte[1000];
+					DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+					try{
+						socket.receive(reply);
+						losses = 0;
+					}catch(SocketTimeoutException e){
+						losses++;
+						System.out.println("Did not received answer " + losses);
+					}
+					System.out.println("Recebeu: " + new String(reply.getData(), 0, reply.getLength()));
+				}
+			}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+			}catch (IOException e){System.out.println("IO: " + e.getMessage());
+			}catch (InterruptedException e){System.out.println("Thread Cannot Sleep");
+			}finally {if(socket != null) socket.close();}
+			}
+		
+	}
+}
