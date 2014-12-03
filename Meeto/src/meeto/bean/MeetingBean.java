76fb40bb53597d01e559d69b9b20cbfac3d2f9f7
OV3RH34T <rjfcasaleiro@gmail.com>
2014-12-03 15:10:56 +0000

    MeetingBean done

--- Meeto/src/meeto/bean/MeetingBean.java
+++ Meeto/src/meeto/bean/MeetingBean.java
@@ -8,15 +8,13 @@ import java.util.Map;
 import meeto.garbage.DatabaseInterface;
 import meeto.garbage.Meeting;
 import meeto.garbage.Meetings;
+import meeto.garbage.User;
 
 public class MeetingBean {
 	private final String databaseIP = "localhost";
 	private final int databasePort = 1200;
 	
 	private Meetings meetings;
-	private int meetingid;
-	
-
 	private Meeting meeting;
 	
 	private DatabaseInterface database;
@@ -33,7 +31,7 @@ public class MeetingBean {
 	}
 	
 	
-	public Meetings getAllMeetings(){
+	public Meetings getAllMeetings(int userid){
 		try {
 			meetings = database.getMeetings((int)session.get("iduser"));
 		} catch (RemoteException e) {
@@ -43,7 +41,7 @@ public class MeetingBean {
 	}
 
 	
-	public Meeting getMeeting(){
+	public Meeting getMeeting(int meetingid){
 		try {
 			meeting = database.getMeeting(meetingid);
 		} catch (RemoteException e) {
@@ -52,12 +50,45 @@ public class MeetingBean {
 		return meeting;
 	}
 	
+	public int createMeeting(String title, String description, String datetime, String location, User leader){
+		Meeting mt = new Meeting(-1,title,description,datetime,location,leader,1);
+		try {
+			database.insertMeeting(mt);
+			return 0;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int editMeeting(int idmeeting, String title, String description, String datetime, String location, User leader){
+		Meeting mt = new Meeting(idmeeting,title,description,datetime,location,leader,1);
+		try {
+			database.updateMeeting(mt);
+			return 0;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int closeMeeting(int idmeeting){
+		Meeting mt = new Meeting();
+		mt.idmeeting=idmeeting;
+		try {
+			database.finishMeeting(mt);
+			return 0;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
 	public void setSession(Map<String, Object> s){
 		session = s;
 	}
 	
-	public void setMeetingid(int meetingid) {
-		this.meetingid = meetingid;
-	}
-
 }
